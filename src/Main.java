3fa7023c3a5631d731495a7b1190dd50653b67df
OmiSeerDev <omarmtinajero@gmail.com>
2023-08-06 20:21:25 -0600

    Adding a watch fav options

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,17 @@ import static java.lang.System.exit;
 public class Main {
     public static void main (String[] args) throws IOException {
         int opcionMenu = -1;
-        String[] botones = {" 1. ver gatos", "2. salir"};
+        String[] botones = {" 1. See cats", "2. See favourites","3. Exit"};
 
         do{
             // main menu
-            String opcion = (String) JOptionPane.showInputDialog(null, "Java Cats", "Main Menu", JOptionPane.INFORMATION_MESSAGE,
-                    null, botones,botones[0]);
-
+            String opcion = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Java Cats",
+                    "Main Menu",
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null, botones,botones[0]
+            );
             //validamos que opcion selecciona el usuario
             for(int i=0;i<botones.length;i++){
                 if(opcion.equals(botones[i])){
